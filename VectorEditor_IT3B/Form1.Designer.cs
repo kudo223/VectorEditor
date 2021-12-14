ee9f51035ec0ec107977e27a95b1aff4516501ed
Dominik Kunc <Kunc.Dominik@stud.vassboskovice.cz>
2021-12-14 12:15:27 +0100

    Přidejte soubory projektu.

---
+++ VectorEditor_IT3B/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace VectorEditor_IT3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pboxCanvas = new System.Windows.Forms.PictureBox();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCanvas)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pboxCanvas
+            // 
+            this.pboxCanvas.BackColor = System.Drawing.Color.White;
+            this.pboxCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pboxCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pboxCanvas.Location = new System.Drawing.Point(134, 0);
+            this.pboxCanvas.Name = "pboxCanvas";
+            this.pboxCanvas.Size = new System.Drawing.Size(860, 590);
+            this.pboxCanvas.TabIndex = 0;
+            this.pboxCanvas.TabStop = false;
+            this.pboxCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pboxCanvas_Paint);
+            this.pboxCanvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pboxCanvas_MouseClick);
+            this.pboxCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pboxCanvas_MouseMove);
+            // 
+            // sfd
+            // 
+            this.sfd.FileOk += new System.ComponentModel.CancelEventHandler(this.sfd_FileOk);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            this.ofd.FileOk += new System.ComponentModel.CancelEventHandler(this.ofd_FileOk);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLine);
+            this.panel1.Controls.Add(this.btnPoint);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(134, 590);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(12, 52);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(112, 34);
+            this.btnLine.TabIndex = 0;
+            this.btnLine.Text = "Úsečka";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Location = new System.Drawing.Point(12, 12);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(112, 34);
+            this.btnPoint.TabIndex = 0;
+            this.btnPoint.Text = "Bod";
+            this.btnPoint.UseVisualStyleBackColor = true;
+            this.btnPoint.Click += new System.EventHandler(this.btnPoint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 590);
+            this.Controls.Add(this.pboxCanvas);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCanvas)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pboxCanvas;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnPoint;
+    }
+}
+
---
+++ VectorEditor_IT3B/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace VectorEditor_IT3B
+{
+    public partial class Form1 : Form
+    {
+        Shapes selectedShape = Shapes.None;
+        List<Shape> shapes;
+        System.Drawing.Point mousePoint;
+        Color defaultButtonColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            defaultButtonColor = btnLine.BackColor;
+            shapes = new List<Shape>();
+        }
+
+        private void pboxCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
+            mousePoint = e.Location;
+            //pboxCanvas.Refresh();
+        }
+
+        private void pboxCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            shapes.Add(new Point(e.Location.X,e.Location.Y));
+            pboxCanvas.Refresh();
+        }
+
+        private void pboxCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw(e.Graphics);
+            }
+        }
+
+        private void sfd_FileOk(object sender, CancelEventArgs e)
+        {
+            var json = JsonConvert.SerializeObject(shapes, Formatting.Indented);
+            File.WriteAllText(sfd.FileName, json);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.S && e.Control)
+            {
+                sfd.ShowDialog();
+            }
+            else if (e.KeyCode == Keys.O && e.Control)
+            {
+                ofd.ShowDialog();
+            }
+        }
+
+        private void ofd_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                var json = File.ReadAllText(ofd.FileName);
+                shapes = JsonConvert.DeserializeObject<List<Shape>>(json);
+                pboxCanvas.Refresh();
+            }
+            catch
+            {
+                MessageBox.Show("Nepodařilo se otevřít.");
+            }
+        }
+
+        private void btnPoint_Click(object sender, EventArgs e)
+        {
+            selectedShape = Shapes.Point;
+            btnPoint.BackColor = Color.Lime;
+            btnLine.BackColor = defaultButtonColor;
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            selectedShape = Shapes.Line;
+            btnPoint.BackColor = defaultButtonColor; 
+            btnLine.BackColor = Color.Lime;
+        }
+    }
+}
